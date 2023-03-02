442b684eb1360b6a369240c8187697c08c6f8851
pscherios <99372151+PsChecha@users.noreply.github.com>
2023-03-02 10:20:25 -0600

    Segundo commit
    Se agregaron los sistemas de creación de usuarios, proximamente se implementara la función de chat

--- app/src/main/java/chat/mouse/MainActivity.java
+++ app/src/main/java/chat/mouse/MainActivity.java
@@ -1,5 +1,6 @@
 package chat.mouse;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -7,6 +8,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,20 +36,60 @@ public class MainActivity extends AppCompatActivity {
 
         txtLoginInfo=findViewById(R.id.txtLoginInfo);
 
-
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(edtEmail.getText().toString().isEmpty() || edtPassword.getText().toString().isEmpty()){
+                    if(isSigningUp && edtUsername.getText().toString().isEmpty()){
+                        Toast.makeText(MainActivity.this, "Faltan datos", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                }
+                if(isSigningUp){
+                    handleSignUp();
+                }else{
+                    handleLogin();
+                }
+            }
+        });
         txtLoginInfo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if(isSigningUp){
                     isSigningUp=false;
                     edtUsername.setVisibility(View.GONE);
-                    btnSubmit.setText("Iniciar Sesion");
+                    btnSubmit.setText("Iniciar Sesión");
                     txtLoginInfo.setText("No tienes una cuenta? Registrate");
                 }else{
                     isSigningUp=true;
                     edtUsername.setVisibility(View.VISIBLE);
                     btnSubmit.setText("Registrarse");
-                    txtLoginInfo.setText("Ya tienes una cuenta? Inicia sesion");
+                    txtLoginInfo.setText("Ya tienes una cuenta? Inicia sesión");
+                }
+            }
+        });
+
+    }
+    private void handleSignUp(){
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(edtEmail.getText().toString(),edtPassword.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(MainActivity.this, "Registro exitoso", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(MainActivity.this, task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+    private void handleLogin(){
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(edtEmail.getText().toString(),edtPassword.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(MainActivity.this, "Sesión Iniciada", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(MainActivity.this, task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
                 }
             }
         });
