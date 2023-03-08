6fbcbc0cf3676dbc17a11a9233e3420db24a1bd2
pscherios <99372151+PsChecha@users.noreply.github.com>
2023-03-08 11:06:13 -0600

    Primer Commit v2
    Se comienza el proyecto nuevamente

--- app/src/main/java/chat/mouse/MainActivity.java
+++ app/src/main/java/chat/mouse/MainActivity.java
@@ -18,83 +18,31 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText edtUsername,edtPassword,edtEmail;
-    private Button btnSubmit;
-    private TextView txtLoginInfo;
-
-    private boolean isSigningUp=true;
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        edtEmail=findViewById(R.id.edtEmail);
-        edtPassword=findViewById(R.id.edtPassword);
-        edtUsername=findViewById(R.id.edtUsername);
-
-        btnSubmit=findViewById(R.id.btnSubmit);
-
-        txtLoginInfo=findViewById(R.id.txtLoginInfo);
-
-        btnSubmit.setOnClickListener(new View.OnClickListener() {
+        Thread thread = new Thread(){
             @Override
-            public void onClick(View view) {
-                if(edtEmail.getText().toString().isEmpty() || edtPassword.getText().toString().isEmpty()){
-                    if(isSigningUp && edtUsername.getText().toString().isEmpty()){
-                        Toast.makeText(MainActivity.this, "Faltan datos", Toast.LENGTH_SHORT).show();
-                        return;
-                    }
+            public void run() {
+                try{
+                    sleep(2000);
+                }catch(Exception e){
+                    e.printStackTrace();
                 }
-                if(isSigningUp){
-                    handleSignUp();
-                }else{
-                    handleLogin();
+                finally {
+                    Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                    startActivity(intent);
                 }
             }
-        });
-        txtLoginInfo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(isSigningUp){
-                    isSigningUp=false;
-                    edtUsername.setVisibility(View.GONE);
-                    btnSubmit.setText("Iniciar Sesión");
-                    txtLoginInfo.setText("No tienes una cuenta? Registrate");
-                }else{
-                    isSigningUp=true;
-                    edtUsername.setVisibility(View.VISIBLE);
-                    btnSubmit.setText("Registrarse");
-                    txtLoginInfo.setText("Ya tienes una cuenta? Inicia sesión");
-                }
-            }
-        });
-
+        };
+        thread.start();
     }
-    private void handleSignUp(){
-        FirebaseAuth.getInstance().createUserWithEmailAndPassword(edtEmail.getText().toString(),edtPassword.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if(task.isSuccessful()){
-                    startActivity(new Intent(MainActivity.this,AmigosActivity.class));
-                    Toast.makeText(MainActivity.this, "Registro exitoso", Toast.LENGTH_SHORT).show();
-                }else{
-                    Toast.makeText(MainActivity.this, task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-    }
-    private void handleLogin(){
-        FirebaseAuth.getInstance().signInWithEmailAndPassword(edtEmail.getText().toString(),edtPassword.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if(task.isSuccessful()){
-                    startActivity(new Intent(MainActivity.this,AmigosActivity.class));
-                    Toast.makeText(MainActivity.this, "Sesión Iniciada", Toast.LENGTH_SHORT).show();
-                }else{
-                    Toast.makeText(MainActivity.this, task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
     }
 }
