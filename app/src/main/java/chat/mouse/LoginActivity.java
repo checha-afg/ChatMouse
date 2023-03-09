9dd0d8c0fc1add885a0017e08da1807b05fc4862
pscherios <99372151+PsChecha@users.noreply.github.com>
2023-03-09 11:18:31 -0600

    Modificaciones
    Se cambia la creacion de usuarios de "correo electronico" a "numero telefonico"
    
    Tambien se realizan cambios de interfaz

--- app/src/main/java/chat/mouse/LoginActivity.java
+++ app/src/main/java/chat/mouse/LoginActivity.java
@@ -3,12 +3,22 @@ package chat.mouse;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText numero,codigo;
+    private Button enviar_numero,enviar_codigo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        numero=(EditText)findViewById(R.id.numero);
+        codigo=(EditText)findViewById(R.id.codigo);
+
+        enviar_numero=(Button)findViewById(R.id.enviar_numero);
+        enviar_codigo=(Button)findViewById(R.id.enviar_codigo);
     }
 }
