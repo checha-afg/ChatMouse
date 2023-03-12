5391c1bde6c34f77650635b1ae8ae37a7b1f0068
pscherios <99372151+PsChecha@users.noreply.github.com>
2023-03-12 13:41:52 -0600

    SISTEMA DE USUARIOS CREADO

--- app/src/main/java/com/chatmouse/app/activities/MainActivity.java
+++ app/src/main/java/com/chatmouse/app/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.chatmouse.app;
+package com.chatmouse.app.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.chatmouse.app.R;
 
 public class MainActivity extends AppCompatActivity {
 
