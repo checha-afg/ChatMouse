eebac8f3dc572b6c63432235aac9b0f8454f86b0
Viru154 <nerygalvezh@gmail.com>
2023-03-14 15:45:38 -0600

    Actualizacion sistema de inicio de Sesion
    Se hicieron los siguientes cambios:
    -Actualizacion del boton de inicio de sesion
    -Se enlazo el boton a la Firebase
    -se agrego una animacion de carga al inicio de sesion
    -Se implemento el inicio
    -Se agrego un Toast que informa de uin inicio de sesion incorrecto

--- app/src/main/java/com/chatmouse/app/activities/SignInActivity.java
+++ app/src/main/java/com/chatmouse/app/activities/SignInActivity.java
@@ -3,21 +3,30 @@ package com.chatmouse.app.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
+import android.view.View;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.chatmouse.app.databinding.ActivitySignInBinding;
+import com.chatmouse.app.utilities.Constants;
+import com.chatmouse.app.utilities.PreferenceManager;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import org.checkerframework.checker.regex.qual.PartialRegex;
 
 import java.util.regex.Pattern;
 
 public class SignInActivity extends AppCompatActivity {
 
     private ActivitySignInBinding binding;
+    private PreferenceManager preferenceManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        preferenceManager = new PreferenceManager(getApplicationContext());
         binding = ActivitySignInBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         setListeners();
@@ -48,7 +57,38 @@ public class SignInActivity extends AppCompatActivity {
                 });
     }*/
     private  void signIn() {
+        loading(true);
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        database.collection(Constants.KEY_COLLECTION_USERS)
+                .whereEqualTo(Constants.KEY_EMAIL, binding.inputEmail.getText().toString())
+                .whereEqualTo(Constants.KEY_PASSWORD, binding.inputPassword.getText().toString())
+                .get()
+                .addOnCompleteListener(task -> {
+                   if(task.isSuccessful() && task.getResult() != null
+                   && task.getResult().getDocuments().size() > 0) {
+                       DocumentSnapshot documentSnapshot = task.getResult().getDocuments().get(0);
+                       preferenceManager.putBoolean(Constants.KEY_IS_SIGNED_IN, true);
+                       preferenceManager.putString(Constants.KEY_USER_ID, documentSnapshot.getId());
+                       preferenceManager.putString(Constants.KEY_NAME, documentSnapshot.getString(Constants.KEY_NAME));
+                       preferenceManager.putString(Constants.KEY_IMAGE, documentSnapshot.getString(Constants.KEY_IMAGE));
+                       Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                       intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                       startActivity(intent);
+                   }else {
+                       loading(false);
+                       showToast("No se logro iniciar sesion :(");
+                   }
+                });
+    }
 
+    private void loading(Boolean isLoading) {
+        if (isLoading){
+            binding.ButtonSignIn.setVisibility(View.INVISIBLE);
+            binding.progressBar.setVisibility(View.VISIBLE);
+        }else{
+            binding.progressBar.setVisibility(View.INVISIBLE);
+            binding.ButtonSignIn.setVisibility(View.VISIBLE);
+        }
     }
 
     private void showToast(String message){
